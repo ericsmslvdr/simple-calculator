971f3254631ab2a96e70f9ba8732002d6e981686
Eric Samuel Salvador <85216886+programmericoder@users.noreply.github.com>
2022-07-02 17:53:13 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const outputBox = document.querySelector('#output')
+const numberBtn = document.querySelectorAll('.number')
+const operationBtn = document.querySelectorAll('.operation')
+const resetBtn = document.querySelector('.reset')
+let operation = ''
+let numTemp = 0
+let total = 0
+
+resetBtn.addEventListener('click', () => {
+    outputBox.value = ''
+    operation = ''
+    numTemp = 0
+    total = 0
+})
+
+numberBtn.forEach((num) => {
+    num.addEventListener('click', () => {
+        outputBox.value += num.value
+    })
+})
+
+operationBtn.forEach((op) => {
+    op.addEventListener('click', () => {
+        switch (op.value) {
+            case '/':
+                operation = '/'
+                numTemp = outputBox.value
+                outputBox.value = ''
+                break;
+
+            case '*':
+                operation = '*'
+                numTemp = outputBox.value
+                outputBox.value = ''
+                break;
+
+            case '-':
+                operation = '-'
+                numTemp = outputBox.value
+                outputBox.value = ''
+                break;
+
+            case '+':
+                operation = '+'
+                numTemp = outputBox.value
+                outputBox.value = ''
+                break;
+
+            case '=':
+                total = outputBox.value
+                if (operation == '-') {
+                    total = numTemp - total
+                } else if (operation == '+') {
+                    total = Number(total) + Number(numTemp)
+                } else if (operation == '*') {
+                    total *= numTemp
+                } else if (operation == '/') {
+                    total = numTemp / total
+                }
+                outputBox.value = total
+                break;
+
+            default:
+                break;
+        }
+    })
+})
+
+
+
+
